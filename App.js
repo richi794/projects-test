8b539b71ac7a307d71b92d18361652afdc8c84ac
richi794 <zocchi.riccardogmail.com>
2021-05-03 16:50:15 +0200

    esercizio

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import Navbar from './components/navbar';
+function App() {
+  return (
+    <>
+     <Navbar />
+    
+     <div className='container'>
+     <h1>cosa desideri ordinare2?</h1>
+    
+     <div className='row'>
+     </div>
+   </>
+  );
+}
+
+export default App;
